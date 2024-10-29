0de54bae1b0b75447a311809a048d946c03c989d
Sadat Sahib <58975768+ssquadteam@users.noreply.github.com>
2024-10-29 12:42:49 +0600

    Fix Build

--- src/fr/maxlego08/zshop/economy/economies/CoinsEngineEconomy.java
+++ src/fr/maxlego08/zshop/economy/economies/CoinsEngineEconomy.java
@@ -11,21 +11,25 @@ public class CoinsEngineEconomy extends DefaultEconomy {
 
     public CoinsEngineEconomy(Plugin plugin, String name, String currency, String denyMessage, String currencyName) {
         super(name, currency, denyMessage);
+        // Retrieve the currency using CoinsEngineAPI's new method
         this.currency = CoinsEngineAPI.getCurrency(currencyName);
     }
 
     @Override
     public double getMoney(OfflinePlayer offlinePlayer) {
-        return CoinsEngineAPI.getUserData(offlinePlayer.getUniqueId()).getCurrencyData(this.currency).getBalance();
+        // Use getBalance to get the currency balance for the specific user
+        return CoinsEngineAPI.getUserData(offlinePlayer.getUniqueId()).getBalance(this.currency);
     }
 
     @Override
     public void depositMoney(OfflinePlayer offlinePlayer, double value) {
+        // Use addBalance to increase the currency balance for the specific user
         CoinsEngineAPI.getUserData(offlinePlayer.getUniqueId()).addBalance(this.currency, value);
     }
 
     @Override
     public void withdrawMoney(OfflinePlayer offlinePlayer, double value) {
+        // Use removeBalance to decrease the currency balance for the specific user
         CoinsEngineAPI.getUserData(offlinePlayer.getUniqueId()).removeBalance(this.currency, value);
     }
 }
